5a149fddccc4c3bef6182fafabe16bdf516a7ed8
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-07-21 19:24:35 +0300

    Security added

---
+++ SpringJwt/src/main/java/com/Spring_Jwt_Training/SpringJwt/SpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Jwt_Training.SpringJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+
+}
---
+++ SpringJwt/src/test/java/com/Spring_Jwt_Training/SpringJwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Jwt_Training.SpringJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
