36e9e88ce9b115336d4e285955440edbeacffc7b
mehmetakifsalman <ahmetakifsalman@gmail.com>
2024-07-23 15:56:03 +0300

    Jwt service complicated and spring security added back

--- SpringJwt/src/main/java/com/Spring_Jwt_Training/SpringJwt/entity/User.java
+++ SpringJwt/src/main/java/com/Spring_Jwt_Training/SpringJwt/entity/User.java
@@ -6,15 +6,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Table(name = "users")
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -37,4 +41,39 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
---
+++ SpringJwt/src/main/java/com/Spring_Jwt_Training/SpringJwt/service/JwtService.java
@@ -0,0 +1,103 @@
+package com.Spring_Jwt_Training.SpringJwt.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    //Generate
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>() , userDetails);
+    }
+
+    public String generateToken(Map<String , Object> extractClaims , UserDetails userDetails){
+        return buildToken(extractClaims , userDetails , jwtExpiration);
+    }
+
+
+    private String buildToken(Map<String , Object> extraClaims , UserDetails userDetails, long exprition){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + exprition))
+                .signWith(getSignKey() , SignatureAlgorithm.HS256)
+                .compact();
+    };
+
+    public long getExpirationTime() {
+        return jwtExpiration;
+    }
+
+    //Extract
+    public <T> T  extractClaim(String token  , Function<Claims , T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token , Claims::getSubject);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token , Claims::getExpiration);
+    }
+
+    public boolean isTokenValid(String token , UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+
+
+
+
+
+
+
+
+
+    private Key getSignKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
